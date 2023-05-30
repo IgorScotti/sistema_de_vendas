b9bd1e115b458f747c376388b42f16fee903d3e8
IgorScotti <igorscoti@hotmail.com>
2023-05-30 16:07:37 -0300

    Criação do JpaRepository e da classe de configuração

---
+++ src/main/java/com/cursowebservice/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cursowebservice.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursowebservice.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
