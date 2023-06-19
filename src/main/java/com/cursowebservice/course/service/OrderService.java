be430d1d9dc9b394e4bdba33ec7772deb85ec499
IgorScotti <igorscoti@hotmail.com>
2023-06-19 17:51:18 -0300

    Ajuste na ordem do response e configuracao do enum OrderStatus

---
+++ src/main/java/com/cursowebservice/course/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.cursowebservice.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursowebservice.course.entities.Order;
+import com.cursowebservice.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
